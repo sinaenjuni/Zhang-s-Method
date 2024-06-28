3c8bb0aa14d9fca7d698b6e6860ab116a5260b53
sinaenjuni <y2k4388@gmail.com>
2024-06-28 14:19:49 +0900

    dev get_image function.

--- src/main.py
+++ src/main.py
@@ -1,4 +1,13 @@
-import sys
+import cv2
+
+def get_image(path):
+    return cv2.imread(path, cv2.IMREAD_COLOR)
 
 if __name__ == "__main__":
-    print(sys.version)
+    ch, cw = 9, 7    # checkboard patterns are 9 row and 7 col.
+    square_size = 20 # 20mm
+
+    img = get_image("../data/board0.jpeg")
+
+    cv2.imshow("img", img)
+    cv2.waitKey()
