a86486341de654804d9a13caa7678db176e93d6c
sinaenjuni <y2k4388@gmail.com>
2024-07-04 17:07:49 +0900

    mod: convert absolute path
    image load bug fix

--- src/utils.py
+++ src/utils.py
@@ -17,23 +17,25 @@ def check_file_extension(file:str)->bool:
     # return any([file_list[0].endswith(extension) for extension in extensions])
     return any(map(file.endswith, extensions)) 
 
+def convert_abs_path(path:str)->str:
+    return list(map(os.path.abspath, path))
+
 def find_numbers(s):
     numbers = re.findall(r"\d+", s)
     if numbers:
         return int(numbers[0])
     else:
         return float('inf')
-    
-
 
 @timing_decorator
 def get_img_files(path):
     if os.path.isfile(path):
-        assert check_file_extension(path), f"{file} is not image file."
-        return path
+        assert check_file_extension(path), f"{path} is not image file."
+        return os.path.abspath(path)
     else:
         file_list = os.listdir(path)
-        return sorted(list(filter(check_file_extension, file_list)), key=find_numbers)
+        file_list = sorted(list(filter(check_file_extension, file_list)), key=find_numbers)
+        return convert_abs_path(file_list)
         
 if __name__ == "__main__":
     path = "./data/data0/"
